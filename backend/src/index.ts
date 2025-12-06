f1c85f5e2633a45be2406cadf594d58854c8407d
JayXie9499 <tsum950210@gmail.com>
2025-12-07 05:57:23 +0800

    feat(server): Bootstrap app execution

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { setupMiddleware } from "./middleware";
+import config from "./core/config";
+import logger from "./core/logger";
 
 const app = express();
 
 setupMiddleware(app);
+
+app.listen(config.PORT, () => {
+	logger.info(`Server is running on port ${config.PORT}`);
+});
 
 export default app;
