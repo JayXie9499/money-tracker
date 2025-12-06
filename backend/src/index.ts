cd4d4013bb5f979818e5efe4251bb8aa75333aee
JayXie9499 <tsum950210@gmail.com>
2025-12-07 06:41:44 +0800

    feat(server): Implement graceful shutdown mechanism

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,21 @@ import express from "express";
 import { setupMiddleware } from "./middleware";
 import config from "./core/config";
 import logger from "./core/logger";
+import prisma from "./core/database";
 
 const app = express();
+const shutdown = async () => {
+	await prisma.$disconnect();
+	process.exit(0);
+};
 
 setupMiddleware(app);
 
 app.listen(config.PORT, () => {
 	logger.info(`Server is running on port ${config.PORT}`);
 });
+
+process.on("SIGTERM", () => shutdown());
+process.on("SIGINT", () => shutdown());
 
 export default app;
