56a735c4454176d915bd108aa8a7d4fab7b49a1e
JayXie9499 <tsum950210@gmail.com>
2025-12-07 17:08:00 +0800

    refactor(prisma): Replace custom LogLevel type with Prisma Enum

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,14 +1,6 @@
 import type { TransportStreamOptions } from "winston-transport";
 import type { PrismaClient } from "./generated/prisma/client";
 
-export enum LogLevel {
-	DEBUG = 1,
-	INFO,
-	WARN,
-	ERROR,
-	FATAL
-}
-
 export interface DatabaseTransportOptions extends TransportStreamOptions {
 	prisma: PrismaClient;
 }
